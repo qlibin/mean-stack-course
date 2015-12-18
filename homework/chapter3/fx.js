6def08c90c5566dd6125180ae64d4aadccc6062d
alibin <qlibin@gmail.com>
2015-12-18 11:17:20 +0300

    inject dependency on Config service that reads values from config.json

--- homework/chapter3/fx.js
+++ homework/chapter3/fx.js
@@ -3,9 +3,9 @@ var _ = require('underscore');
 
 // TODO: Make this function depend on the Config service from `dependencies.js`
 // and use Config.openExchangeRatesKey instead of an environment variable.
-module.exports = function() {
+module.exports = function(Config) {
   var url = 'http://openexchangerates.org/api/latest.json?app_id=' +
-    process.env.OPEN_EXCHANGE_RATES_KEY;
+    Config.openExchangeRatesKey;
   var rates = {
     USD: 1,
     EUR: 1.1,
